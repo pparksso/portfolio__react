dd200d9d0b46baef5031f99d81e4fcdb9d3cadb0
pparksso <thgml5143@hanmail.net>
2023-11-20 23:07:46 +0900

    chore: tailwind 적용

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+    purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {},
+        fontFamily: {
+            sans: ['Roboto', 'Noto Sans', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+    },
+    plugins: [],
+};

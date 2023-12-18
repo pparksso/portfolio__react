b22fb67354a88430ebbad379c9467e2e8771eefa
pparksso <thgml5143@hanmail.net>
2023-12-18 13:43:46 +0900

    design: mobile-header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,6 +3,9 @@ import { Link, useLocation } from 'react-router-dom';
 const Header = () => {
     const location = useLocation();
 
+    // 모바일 사이드 헤더
+    const [sideH, setSideH] = useState(false);
+
     // 헤더 높이 조절
     const [isHidden, setIsHidden] = useState(false);
     const [prevScrollPosition, setPrevScrollPosition] = useState(0);
@@ -29,50 +32,151 @@ const Header = () => {
         if (hash && hash.startsWith('#')) {
             const targetId = hash.substring(1);
             const targetEl = document.getElementById(targetId);
-
             if (targetEl) {
                 targetEl.scrollIntoView({ behavior: 'smooth' });
             }
         }
     }, [location]);
 
+    const sideHeaderHandler = () => {
+        setSideH(!sideH);
+    };
     return (
-        <header className={`fixed w-full text-white bg-black z-50 ${isHidden ? 'h-0' : 'h-16'} transition-all`}>
-            <div className="flex items-center justify-center h-full">
-                <h1 className="absolute left-3 -translate-y-2/4 top-2/4">
-                    <Link to="/">☀️ Dev Sohee</Link>
-                </h1>
-                <nav className="text-xl font-bold">
-                    <ul className="flex">
-                        <li>
-                            <a href="/#profile" className="inline-block p-4">
-                                Profile
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/#career" className="inline-block p-4">
-                                Career
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/#work" className="inline-block p-4">
-                                Experience
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/#project" className="inline-block p-4">
-                                Side Project
-                            </a>
-                        </li>
-                        <li>
-                            <Link to="/contact" className="inline-block p-4">
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+        <>
+            {/* pc */}
+            <header
+                className={`fixed w-full text-white bg-black z-50 ${
+                    isHidden ? 'h-0' : 'h-16'
+                } transition-all hidden md:block lg:block xl:block`}
+            >
+                <div className="flex items-center justify-center h-full">
+                    <h1 className="absolute left-3 -translate-y-2/4 top-2/4">
+                        <Link to="/">☀️ Dev Sohee</Link>
+                    </h1>
+                    <nav className="text-xl font-bold">
+                        <ul className="flex">
+                            <li>
+                                <a href="/#profile" className="inline-block p-4">
+                                    Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/#career" className="inline-block p-4">
+                                    Career
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/#work" className="inline-block p-4">
+                                    Experience
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/#project" className="inline-block p-4">
+                                    Side Project
+                                </a>
+                            </li>
+                            <li>
+                                <Link to="/contact" className="inline-block p-4">
+                                    Contact
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            {/* mobile */}
+            <header className="fixed top-0 left-0 z-50 block w-full h-16 bg-white shadow-lg md:hidden lg:hidden xl:hidden">
+                <div className="relative flex items-center justify-between h-full px-8">
+                    <h1 className="text-xl">
+                        <Link to="/">☀️</Link>
+                    </h1>
+                    <div className="relative w-8 h-full">
+                        <button className="relative w-full h-full" onClick={sideHeaderHandler}>
+                            <span
+                                className={`inline-block w-8 bg-black absolute h-[3px] right-0 transition-all duration-200 ${
+                                    sideH ? 'top-1/2' : 'top-[21px]'
+                                } ${sideH && 'rotate-45'}`}
+                            ></span>
+                            <span
+                                className={`inline-block w-8 bg-black absolute h-[3px] top-1/2 right-0 ${
+                                    sideH && 'hidden'
+                                }`}
+                            ></span>
+                            <span
+                                className={`inline-block w-8 bg-black absolute h-[3px] right-0 transition-all duration-200 ${
+                                    sideH ? 'top-1/2' : 'top-[42px]'
+                                } ${sideH && '-rotate-45'}`}
+                            ></span>
+                        </button>
+                    </div>
+                </div>
+                <div
+                    className={`absolute top-[64px] w-[100%] h-[100vh] bg-black bg-opacity-20 transition-all duration-200 ease-linear ${
+                        sideH ? 'right-[0px]' : 'right-[-1000px]'
+                    }`}
+                >
+                    <div className="bg-white w-[80%] h-[100vh] absolute top-0 right-0 p-8">
+                        <ul className="">
+                            <li className="py-2">
+                                <a
+                                    href="/#profile"
+                                    className={`inline-block text-2xl font-bold ${
+                                        location.hash === '#profile' && 'text-emerald'
+                                    }`}
+                                    onClick={() => setSideH(false)}
+                                >
+                                    Profile
+                                </a>
+                            </li>
+                            <li className="py-2">
+                                <a
+                                    href="/#career"
+                                    className={`inline-block text-2xl font-bold ${
+                                        location.hash === '#career' && 'text-emerald'
+                                    }`}
+                                    onClick={() => setSideH(false)}
+                                >
+                                    Career
+                                </a>
+                            </li>
+                            <li className="py-2">
+                                <a
+                                    href="/#work"
+                                    className={`inline-block text-2xl font-bold ${
+                                        location.hash === '#work' && 'text-emerald'
+                                    }`}
+                                    onClick={() => setSideH(false)}
+                                >
+                                    Experience
+                                </a>
+                            </li>
+                            <li className="py-2">
+                                <a
+                                    href="/#project"
+                                    className={`inline-block text-2xl font-bold ${
+                                        location.hash === '#project' && 'text-emerald'
+                                    }`}
+                                    onClick={() => setSideH(false)}
+                                >
+                                    Side Project
+                                </a>
+                            </li>
+                            <li className="py-2">
+                                <Link
+                                    to="/contact"
+                                    className={`inline-block text-2xl font-bold ${
+                                        location.pathname.includes('contact') && 'text-emerald'
+                                    }`}
+                                    onClick={() => setSideH(false)}
+                                >
+                                    Contact
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+        </>
     );
 };
 
